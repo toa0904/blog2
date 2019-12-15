64a77f411e6a9f08c4697c9f92b518685f27bd60
toa0904 <pazutomo0428@icloud.com>
2019-12-15 16:13:00 +0900

    rootingの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/new'
+  resources :articles
+  root to: "articles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
